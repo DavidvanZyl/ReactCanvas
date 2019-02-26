f063a59c8400c8dc9663fa4dd76cd3573257f029
David van Zyl <info@davidvanzyl.io>
2019-02-26 17:56:06 +0000

    Updated packages and cleared some vulerabilities, react-scripts still causing some minors

--- src/Components/ControlPanel/index.js
+++ src/Components/ControlPanel/index.js
@@ -9,7 +9,7 @@ import { createStore } from 'redux';
 import { connect } from 'react-redux';
 
 
-const Container = ({hue, saturation, lightness, red, green, blue, hex, thickness, setHue, setSaturation, setLightness, setThickness}) => {
+const Container = ({ hue, saturation, lightness, red, green, blue, hex, thickness, setHue, setSaturation, setLightness, setThickness }) => {
     return <div id='container'>
         <Hue hue={hue} saturation={saturation} lightness={lightness} setHue={setHue} />
         <Saturation hue={hue} saturation={saturation} lightness={lightness} setSaturation={setSaturation} />
@@ -30,20 +30,19 @@ const Container = ({hue, saturation, lightness, red, green, blue, hex, thickness
 }
 
 class Hue extends React.Component {
-    constructor({hue, saturation, lightness, thickness, setHue}) {
-        super({hue, saturation, lightness, thickness, setHue} );
+    constructor({ hue, saturation, lightness, thickness, setHue }) {
+        super({ hue, saturation, lightness, thickness, setHue });
 
         const padding = 60;
         const innerSize = 300;
-        this.radius = innerSize/2;
+        this.radius = innerSize / 2;
         this.outerSize = innerSize + padding;
-        this.centerOffset = this.outterSize/2;
+        this.centerOffset = this.outterSize / 2;
 
         this.state = {
             dragging: false
         }
 
-        // These are set in the render method
         this.canvas = null;
         this.selector = null;
     }
@@ -51,12 +50,12 @@ class Hue extends React.Component {
     render() {
         return (
             <svg ref={(canvas) => { this.canvas = canvas; }}
-                 width={this.outerSize} height={this.outerSize}
-                 viewBox={`0 0 ${this.outerSize} ${this.outerSize}`}
-                 xmlns="http://www.w3.org/2000/svg" version="1.1">
+                width={this.outerSize} height={this.outerSize}
+                viewBox={`0 0 ${this.outerSize} ${this.outerSize}`}
+                xmlns="http://www.w3.org/2000/svg" version="1.1">
 
                 <g transform="translate(180,180)">
-                    {Array.from({length: 360}, (value, key) => (
+                    {Array.from({ length: 360 }, (value, key) => (
                         <HueSlice
                             key={key}
                             degree={key}
@@ -69,7 +68,7 @@ class Hue extends React.Component {
                             degree={this.props.hue}
                             radius={this.radius}
                             color={this.state.dragging ? `hsl(${this.props.hue}, ${this.props.saturation}%, ${this.props.lightness}%)` : "white"}
-                            marker={true}/>
+                            marker={true} />
                     </g>
                     <text
                         x="10"
@@ -106,33 +105,33 @@ class Hue extends React.Component {
         let touchEnds = Rx.Observable.fromEvent(this.canvas, "touchend");
 
         let mouseDrags = mouseDowns.concatMap(clickEvent => {
-            const xMouseShouldBe = Math.sin(this.props.hue/180*Math.PI)*this.radius;
-            const yMouseShouldBe = -Math.cos(this.props.hue/180*Math.PI)*this.radius;
+            const xMouseShouldBe = Math.sin(this.props.hue / 180 * Math.PI) * this.radius;
+            const yMouseShouldBe = -Math.cos(this.props.hue / 180 * Math.PI) * this.radius;
             const xMouseIs = clickEvent.clientX;
             const yMouseIs = clickEvent.clientY;
             const xMouseDelta = xMouseIs - xMouseShouldBe;
             const yMouseDelta = yMouseIs - yMouseShouldBe;
             return mouseMoves.takeUntil(mouseUps.merge(mouseLeaves)).map(moveEvent => {
-                const xRelativeToCenter = moveEvent.clientX-xMouseDelta;
-                const yRelativeToCenter = moveEvent.clientY-yMouseDelta;
-                const degree = Math.atan(yRelativeToCenter/xRelativeToCenter)*180/Math.PI + 90 + (xRelativeToCenter>=0 ? 0 : 180);
+                const xRelativeToCenter = moveEvent.clientX - xMouseDelta;
+                const yRelativeToCenter = moveEvent.clientY - yMouseDelta;
+                const degree = Math.atan(yRelativeToCenter / xRelativeToCenter) * 180 / Math.PI + 90 + (xRelativeToCenter >= 0 ? 0 : 180);
                 return parseInt(degree, 10);
             })
         });
 
         let touchDrags = touchStarts.concatMap(startEvent => {
             startEvent.preventDefault();
-            const xTouchShouldBe = Math.sin(this.props.hue/180*Math.PI)*this.radius;
-            const yTouchShouldBe = -Math.cos(this.props.hue/180*Math.PI)*this.radius;
+            const xTouchShouldBe = Math.sin(this.props.hue / 180 * Math.PI) * this.radius;
+            const yTouchShouldBe = -Math.cos(this.props.hue / 180 * Math.PI) * this.radius;
             const xTouchIs = startEvent.touches[0].clientX;
             const yTouchIs = startEvent.touches[0].clientY;
             const xTouchDelta = xTouchIs - xTouchShouldBe;
             const yTouchDelta = yTouchIs - yTouchShouldBe;
             return touchMoves.takeUntil(touchEnds).map(moveEvent => {
                 moveEvent.preventDefault();
-                const xRelativeToCenter = moveEvent.touches[0].clientX-xTouchDelta;
-                const yRelativeToCenter = moveEvent.touches[0].clientY-yTouchDelta;
-                const degree = Math.atan(yRelativeToCenter/xRelativeToCenter)*180/Math.PI + 90 + (xRelativeToCenter>=0 ? 0 : 180);
+                const xRelativeToCenter = moveEvent.touches[0].clientX - xTouchDelta;
+                const yRelativeToCenter = moveEvent.touches[0].clientY - yTouchDelta;
+                const degree = Math.atan(yRelativeToCenter / xRelativeToCenter) * 180 / Math.PI + 90 + (xRelativeToCenter >= 0 ? 0 : 180);
                 return parseInt(degree, 10);
             })
         });
@@ -142,7 +141,7 @@ class Hue extends React.Component {
         let dragEnds = mouseUps.merge(mouseLeaves).merge(touchEnds);
 
         dragStarts.forEach(() => {
-            this.setState({dragging: true});
+            this.setState({ dragging: true });
         });
 
         drags.forEach(degree => {
@@ -150,27 +149,27 @@ class Hue extends React.Component {
         });
 
         dragEnds.forEach(() => {
-            this.setState({dragging: false});
+            this.setState({ dragging: false });
         });
     }
 }
 
-const HueSlice = ({degree, color, radius, marker}) => {
+const HueSlice = ({ degree, color, radius, marker }) => {
     const thickness = marker ? 5 : 1;
-    const startX  =   Math.sin((degree-thickness)/180*Math.PI)*radius;
-    const startY  = - Math.cos((degree-thickness)/180*Math.PI)*radius;
-    const endX    =   Math.sin((degree+thickness)/180*Math.PI)*radius;
-    const endY    = - Math.cos((degree+thickness)/180*Math.PI)*radius;
+    const startX = Math.sin((degree - thickness) / 180 * Math.PI) * radius;
+    const startY = - Math.cos((degree - thickness) / 180 * Math.PI) * radius;
+    const endX = Math.sin((degree + thickness) / 180 * Math.PI) * radius;
+    const endY = - Math.cos((degree + thickness) / 180 * Math.PI) * radius;
     return <path
         className={marker ? 'marker' : ''}
         d={`M ${startX} ${startY} A ${radius} ${radius} 0 0 1 ${endX} ${endY}`}
         stroke={color} />
 }
 
-const Saturation = ({hue, saturation, lightness, setSaturation}) => {
+const Saturation = ({ hue, saturation, lightness, setSaturation }) => {
     const gradient = <linearGradient id="Saturation" x1="0" x2="0" y1="0" y2="1">
-        <stop offset="0%" stopColor={`hsl(${hue}, 100%, ${lightness}%)`}/>
-        <stop offset="100%" stopColor={`hsl(${hue}, 0%, ${lightness}%)`}/>
+        <stop offset="0%" stopColor={`hsl(${hue}, 100%, ${lightness}%)`} />
+        <stop offset="100%" stopColor={`hsl(${hue}, 0%, ${lightness}%)`} />
     </linearGradient>;
     return <Percentage
         type="Saturation" value={saturation} gradient={gradient}
@@ -178,11 +177,11 @@ const Saturation = ({hue, saturation, lightness, setSaturation}) => {
         set={setSaturation} />
 }
 
-const Lightness = ({hue, saturation, lightness, setLightness}) => {
+const Lightness = ({ hue, saturation, lightness, setLightness }) => {
     const gradient = <linearGradient id="Lightness" x1="0" x2="0" y1="0" y2="1">
-        <stop offset="0%" stopColor={`hsl(${hue}, ${saturation}%, 100%)`}/>
-        <stop offset="50%" stopColor={`hsl(${hue}, ${saturation}%, 50%)`}/>
-        <stop offset="100%" stopColor={`hsl(${hue}, ${saturation}%, 0%)`}/>
+        <stop offset="0%" stopColor={`hsl(${hue}, ${saturation}%, 100%)`} />
+        <stop offset="50%" stopColor={`hsl(${hue}, ${saturation}%, 50%)`} />
+        <stop offset="100%" stopColor={`hsl(${hue}, ${saturation}%, 0%)`} />
     </linearGradient>;
     return <Percentage
         type="Lightness" value={lightness} gradient={gradient}
@@ -190,10 +189,10 @@ const Lightness = ({hue, saturation, lightness, setLightness}) => {
         set={setLightness} />
 }
 
-const Thickness = ({hue, saturation, lightness, thickness, setThickness}) => {
+const Thickness = ({ hue, saturation, lightness, thickness, setThickness }) => {
     const gradient = <linearGradient id="Thickness" x1="0" x2="0" y1="0" y2="1">
-        <stop offset="0%" stopColor={`hsl(${hue}, ${saturation}%, ${lightness}%)`}/>
-        <stop offset="100%" stopColor={`hsl(${hue}, ${saturation}%, ${lightness}%)`}/>
+        <stop offset="0%" stopColor={`hsl(${hue}, ${saturation}%, ${lightness}%)`} />
+        <stop offset="100%" stopColor={`hsl(${hue}, ${saturation}%, ${lightness}%)`} />
     </linearGradient>;
     return <Percentage
         type="Thickness" value={thickness} gradient={gradient}
@@ -202,11 +201,11 @@ const Thickness = ({hue, saturation, lightness, thickness, setThickness}) => {
 }
 
 class Percentage extends React.Component {
-    constructor({type, value, gradient, hue, saturation, lightness, thickness, set}) {
-        super({type, value, gradient, hue, saturation, lightness, thickness, set});
+    constructor({ type, value, gradient, hue, saturation, lightness, thickness, set }) {
+        super({ type, value, gradient, hue, saturation, lightness, thickness, set });
 
         const padding = 60;
-        this.padding = padding/2;
+        this.padding = padding / 2;
         const innerSize = 150;
         this.innerSize = innerSize;
         this.outterSize = innerSize + padding;
@@ -216,7 +215,6 @@ class Percentage extends React.Component {
             dragging: false
         }
 
-        // These are set in the render method
         this.canvas = null;
         this.selector = null;
     }
@@ -224,22 +222,22 @@ class Percentage extends React.Component {
     render() {
         return (
             <svg ref={(canvas) => { this.canvas = canvas; }}
-                 width={this.outterSize} height={this.outterSize}
-                 viewBox={`0 0 ${this.outterSize} ${this.outterSize}`}
-                 xmlns="http://www.w3.org/2000/svg" version="1.1">
+                width={this.outterSize} height={this.outterSize}
+                viewBox={`0 0 ${this.outterSize} ${this.outterSize}`}
+                xmlns="http://www.w3.org/2000/svg" version="1.1">
                 <defs>
                     {this.props.gradient}
                 </defs>
                 <g transform={`translate(${this.padding},${this.padding})`}>
                     <rect x={this.barOffsetX} y="0"
-                          width="20" height={this.innerSize}
-                          strokeWidth="20"
-                          fill={`url(#${this.props.type})`}/>
+                        width="20" height={this.innerSize}
+                        strokeWidth="20"
+                        fill={`url(#${this.props.type})`} />
                     <g ref={(selector) => { this.selector = selector; }}>
-                          <rect x={this.barOffsetX-10} y={this.innerSize*(1-this.props.value/100) -25/2}
-                              width="40" height="25"
-                              strokeWidth="20"
-                              fill={this.state.dragging ? `hsl(${this.props.hue}, ${this.props.saturation}%, ${this.props.lightness}%)` : "white"}/>
+                        <rect x={this.barOffsetX - 10} y={this.innerSize * (1 - this.props.value / 100) - 25 / 2}
+                            width="40" height="25"
+                            strokeWidth="20"
+                            fill={this.state.dragging ? `hsl(${this.props.hue}, ${this.props.saturation}%, ${this.props.lightness}%)` : "white"} />
                     </g>
                     <text
                         x="40"
@@ -276,12 +274,12 @@ class Percentage extends React.Component {
         let touchEnds = Rx.Observable.fromEvent(this.canvas, "touchend");
 
         let mouseDrags = mouseDowns.concatMap(clickEvent => {
-            const yMouseShouldBe = (1-this.props.value/100)*this.innerSize;
+            const yMouseShouldBe = (1 - this.props.value / 100) * this.innerSize;
             const yMouseIs = clickEvent.clientY;
             const yMouseDelta = yMouseIs - yMouseShouldBe;
             return mouseMoves.takeUntil(mouseUps.merge(mouseLeaves)).map(moveEvent => {
-                const y = moveEvent.clientY-yMouseDelta;
-                let percentage = (1-y/this.innerSize)*100;
+                const y = moveEvent.clientY - yMouseDelta;
+                let percentage = (1 - y / this.innerSize) * 100;
                 percentage = Math.min(percentage, 100);
                 percentage = Math.max(percentage, 0);
                 return parseInt(percentage, 10);
@@ -290,13 +288,13 @@ class Percentage extends React.Component {
 
         let touchDrags = touchStarts.concatMap(startEvent => {
             startEvent.preventDefault();
-            const yTouchShouldBe = (1-this.props.value/100)*this.innerSize;
+            const yTouchShouldBe = (1 - this.props.value / 100) * this.innerSize;
             const yTouchIs = startEvent.touches[0].clientY;
             const yTouchDelta = yTouchIs - yTouchShouldBe;
             return touchMoves.takeUntil(touchEnds).map(moveEvent => {
                 moveEvent.preventDefault();
-                const y = moveEvent.touches[0].clientY-yTouchDelta;
-                let percentage = (1-y/this.innerSize)*100;
+                const y = moveEvent.touches[0].clientY - yTouchDelta;
+                let percentage = (1 - y / this.innerSize) * 100;
                 percentage = Math.min(percentage, 100);
                 percentage = Math.max(percentage, 0);
                 return parseInt(percentage, 10);
@@ -308,7 +306,7 @@ class Percentage extends React.Component {
         let dragEnds = mouseUps.merge(mouseLeaves).merge(touchEnds);
 
         dragStarts.forEach(() => {
-            this.setState({dragging: true});
+            this.setState({ dragging: true });
         });
 
         drags.forEach(percentage => {
@@ -316,14 +314,10 @@ class Percentage extends React.Component {
         });
 
         dragEnds.forEach(() => {
-            this.setState({dragging: false});
+            this.setState({ dragging: false });
         });
     }
 }
-
-/*********
- * REDUX
- **********/
 
 const initialState = {
     hue: 169,
@@ -339,17 +333,17 @@ const initialState = {
 const hsl2rgb = (hue, saturation, lightness) => {
     saturation /= 100;
     lightness /= 100;
-    const C = (1 - Math.abs(2*lightness - 1)) * saturation;
+    const C = (1 - Math.abs(2 * lightness - 1)) * saturation;
     const X = C * (1 - Math.abs((hue / 60) % 2 - 1));
-    const m = lightness - C/2;
+    const m = lightness - C / 2;
     let [R, G, B] =
-    (0 <= hue && hue <  60 && [C, X, 0]) ||
-    (60 <= hue && hue < 120 && [X, C, 0]) ||
-    (120 <= hue && hue < 180 && [0, C, X]) ||
-    (180 <= hue && hue < 240 && [0, X, C]) ||
-    (240 <= hue && hue < 300 && [X, 0, C]) ||
-    (300 <= hue && hue < 360 && [C, 0, X]);
-    [R, G, B] = [(R+m)*255, (G+m)*255, (B+m)*255];
+        (0 <= hue && hue < 60 && [C, X, 0]) ||
+        (60 <= hue && hue < 120 && [X, C, 0]) ||
+        (120 <= hue && hue < 180 && [0, C, X]) ||
+        (180 <= hue && hue < 240 && [0, X, C]) ||
+        (240 <= hue && hue < 300 && [X, 0, C]) ||
+        (300 <= hue && hue < 360 && [C, 0, X]);
+    [R, G, B] = [(R + m) * 255, (G + m) * 255, (B + m) * 255];
     return [Math.round(R), Math.round(G), Math.round(B)];
 }
 
@@ -357,9 +351,9 @@ const rgb2hex = (red, green, blue) => {
     red = red.toString(16).toUpperCase();
     green = green.toString(16).toUpperCase();
     blue = blue.toString(16).toUpperCase();
-    red = red.length === 1 ? "0"+red : red;
-    green = green.length === 1 ? "0"+green : green;
-    blue = blue.length === 1 ? "0"+blue : blue;
+    red = red.length === 1 ? "0" + red : red;
+    green = green.length === 1 ? "0" + green : green;
+    blue = blue.length === 1 ? "0" + blue : blue;
     return `#${red}${green}${blue}`;
 }
 
@@ -391,13 +385,7 @@ const mainReducer = (state = initialState, action) => {
 };
 
 
-
-
 export const store = createStore(mainReducer);
-
-/*********
- * REACT-REDUX
- **********/
 
 const mapStateToProps = (state, ownProps) => {
     return {
@@ -441,11 +429,5 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     }
 }
 
-/* Import { connect, Provider } from 'react-redux' */
-// const { connect, Provider } = 'react-redux';
-
 export const ConnectedContainer = connect(mapStateToProps, mapDispatchToProps)(Container);
 
-/*********
- * REACT DOM + REDUX
- **********/
